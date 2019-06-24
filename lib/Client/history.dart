ea4c6134e8fe94aa8f5cbe8cca8110221a3fb25e
sentot205 <sentot205@gmail.com>
2019-06-25 05:58:06 +0700

    Test List Builder

--- lib/Client/history.dart
+++ lib/Client/history.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(new MaterialApp(
+
+    home: new History(data: new List<String>.generate(300, (i)=>"Ini data ke $i"),),
+  ));
+}
+
 class History extends StatelessWidget {
+  final List<String> data;
+  History ({this.data});
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text("Riwayat Login"),),
-      body: Center(
-        child: RaisedButton(
-          child: Text("Back"),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-      ),
+        body: new Container(
+          child: new ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index){
+              return new ListTile(
+                leading: new Icon(Icons.widgets),
+                title: new Text("${data[index]}"),
+              );
+            },
+          ),
+        )
     );
   }
 }
