83a9457d8eed9b5bca0e003d9ff496d13a806a86
sentot205 <sentot205@gmail.com>
2019-06-25 14:12:59 +0700

    .

--- lib/Client/history.dart
+++ lib/Client/history.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(new MaterialApp(
-
-    home: new History(data: new List<String>.generate(300, (i)=>"Ini data ke $i"),),
-  ));
-}
-
 class History extends StatelessWidget {
-  final List<String> data;
-  History ({this.data});
-
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text("Riwayat Login"),),
-        body: new Container(
-          child: new ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index){
-              return new ListTile(
-                leading: new Icon(Icons.widgets),
-                title: new Text("${data[index]}"),
-              );
-            },
-          ),
-        )
+        body: _dataListBuilder(),
     );
   }
+}
+
+Widget _dataListBuilder() {
+  final items = List<String>.generate(100, (i) => "Item ke-$i");
+  return new ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return ListTile(
+          title: Text(item),
+        );
+      }
+  );
 }
